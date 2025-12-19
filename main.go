14c43d37acec7360f749e00ee119378ddacc88d0
sanfrwl <ghassan.feriwel@gmail.com>
2025-12-19 16:44:55 +0700

    edit by ghassan

--- main.go
+++ main.go
@@ -7,37 +7,36 @@ import (
 func main() {
 
 	var (
-		harga []int
-		inputHarga int
-		jumlahBarang int
+		harga          []int
+		inputHarga     int
+		jumlahBarang   int
 		totalPembelian int
-		setelahDiskon float32
-		diskon float32
-		// kosong bool 
-		dummy string
+		setelahDiskon  float32
+		diskon         float32
+		dummy          string
 	)
 
 	const (
-		diskon5 float32 = 0.05
+		diskon5  float32 = 0.05
 		diskon10 float32 = 0.10
 		diskon20 float32 = 0.20
 	)
-	
+
 	fmt.Println(">> Selamat datang di GTR MART, silahkan masukkan harga dari setiap barang yang anda beli\n>> Masukkan harga HARUS menggunakan tanda titik (desimal)\n>> Input angka 0.0 untuk menyelesaikan\n")
-	
 
 	fmt.Print("\nMasukkan jumlah barang yang anda beli z: ")
 	jumlahBarang = -1
 	fmt.Scan(&jumlahBarang)
 
-	if jumlahBarang > 1000{
+	if jumlahBarang > 1000 {
+		fmt.Println(">> [ERROR] Jumlah barang tidak boleh lebih dari 1000 ")
 		fmt.Print("Masukkan jumlah barang yang anda beli : ")
 		fmt.Scan(&jumlahBarang)
-	} 
-	
+	}
+
 	if jumlahBarang > 0 && jumlahBarang <= 1000 {
 		totalPembelian = 0
-		i := 0 
+		i := 0
 		for i < jumlahBarang {
 			fmt.Printf("Masukkan harga barang ke - %d : ", i+1)
 			inputHarga = -1
@@ -51,29 +50,26 @@ func main() {
 				fmt.Println("Input harga tidak boleh 0 sebelum semua barang diinput")
 			} else if inputHarga == 1001 {
 				i--
-			}else {
+			} else {
 				fmt.Println("Input harga tidak boleh negatif")
 				fmt.Scanf("%s", &dummy)
 			}
 		}
-	} 
-
-
+	}
 
 	l := len(harga)
-	
-	for i := 0; i < l - 1; i++ {
-		for j := 0; j < l - 1 - i; j++ {
+
+	for i := 0; i < l-1; i++ {
+		for j := 0; j < l-1-i; j++ {
 			if harga[j] > harga[j+1] {
 				harga[j], harga[j+1] = harga[j+1], harga[j]
 			}
 		}
 	}
 
-	
 	// --- Perhitungan Diskon ---
-	
-	if totalPembelian >= 100000 && totalPembelian < 200000  {
+
+	if totalPembelian >= 100000 && totalPembelian < 200000 {
 		diskon = diskon5
 	} else if totalPembelian >= 200000 && totalPembelian < 300000 {
 		diskon = diskon10
@@ -83,7 +79,7 @@ func main() {
 		diskon = 0
 	}
 
-	setelahDiskon = float32(totalPembelian) - (float32(totalPembelian)*diskon)
+	setelahDiskon = float32(totalPembelian) - (float32(totalPembelian) * diskon)
 	setelahDiskon = float32(int(setelahDiskon + 0.5)) // pembulatan ke atas
 
 	fmt.Println("\n=========================================================================================")
