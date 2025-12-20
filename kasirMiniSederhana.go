ec5468e28c686b485112936b2ddbdd56b11de9e6
sanfrwl <ghassan.feriwel@gmail.com>
2025-12-20 16:05:48 +0700

    mantap

--- kasirMiniSederhana.go
+++ kasirMiniSederhana.go
@@ -13,16 +13,16 @@ func main() {
 		totalPembelian int
 		setelahDiskon  float32
 		diskon         float32
-		kosong 		   bool 
+		kosong         bool
 		dummy          string
 	)
 
 	const (
 		batasBarang int     = 1000
-		diskon5  float32 = 0.05
-		diskon10 float32 = 0.10
-		diskon20 float32 = 0.20
-		diskon30 float32 = 0.30
+		diskon5     float32 = 0.05
+		diskon10    float32 = 0.10
+		diskon20    float32 = 0.20
+		diskon30    float32 = 0.30
 	)
 
 	fmt.Println("\n==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==")
@@ -31,7 +31,7 @@ func main() {
 	fmt.Println("==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==")
 	fmt.Println("==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==  ==")
 
-	fmt.Println("\n>> Selamat datang di GTR MART, silahkan masukkan harga dari setiap barang yang anda beli\n>> Masukkan harga tanpa menggunakan tanda titik atau koma (desimal)\n>> Maksimal barang yang dibeli adalah 1000 Item\n>> Masukkan harga 0 (nol) untuk membatalkan pembelian\n")
+	fmt.Println("\n>> Selamat datang di GTR MART, silahkan masukkan harga dari setiap barang yang anda beli\n>> Masukkan harga tanpa menggunakan tanda titik atau koma (desimal)\n>> Maksimal barang yang dibeli adalah 1000 Item\n>> Masukkan harga 0 (nol) untuk membatalkan pembelian")
 
 	// --- Input Jumlah Barang ---
 	fmt.Print("\nMasukkan jumlah barang yang anda beli : ")
@@ -67,54 +67,54 @@ func main() {
 				fmt.Scanf("%s", &dummy)
 			} else if inputHarga == 0 {
 				i = jumlahBarang
-		}
-	}
-
-	// --- Pengurutan Harga (Bubble Sort) ---
-	l := len(harga)
-
-	for i := 0; i < l-1; i++ {
-		for j := 0; j < l-1-i; j++ {
-			if harga[j] > harga[j+1] {
-				harga[j], harga[j+1] = harga[j+1], harga[j]
 			}
 		}
+
+		// --- Pengurutan Harga (Bubble Sort) ---
+		l := len(harga)
+
+		for i := 0; i < l-1; i++ {
+			for j := 0; j < l-1-i; j++ {
+				if harga[j] > harga[j+1] {
+					harga[j], harga[j+1] = harga[j+1], harga[j]
+				}
+			}
+		}
+
+		// --- Perhitungan Diskon ---
+		if totalPembelian >= 100000 && totalPembelian < 200000 {
+			diskon = diskon5
+		} else if totalPembelian >= 200000 && totalPembelian < 300000 {
+			diskon = diskon10
+		} else if totalPembelian >= 300000 && totalPembelian < 500000 {
+			diskon = diskon20
+		} else if totalPembelian >= 500000 {
+			diskon = diskon30
+		} else if totalPembelian == 0 || totalPembelian < 100000 {
+			diskon = 0
+		}
+
+		setelahDiskon = float32(totalPembelian) - (float32(totalPembelian) * diskon)
+		setelahDiskon = float32(int(setelahDiskon + 0.5)) // pembulatan ke atas
+
+		// --- Output ---
+		fmt.Println("\n=========================================================================================")
+		// --- Kondisi Pembatalan Pembelian ---
+		if kosong == true {
+			fmt.Println("\n>> ANDA TIDAK MEMBELI BARANG APAPUN ")
+		} else if inputHarga == 0 {
+			fmt.Println("\n>> PEMBELIAN ANDA DIBATALKAN")
+			harga = nil
+			totalPembelian = 0
+			diskon = 0
+			setelahDiskon = 0
+		}
+
+		fmt.Println("\nUrutan Harga :", harga)
+		fmt.Printf("Total Pembelian Anda : %d\n", totalPembelian)
+		fmt.Println("Diskon yang anda peroleh :", diskon*100, "%")
+		fmt.Printf("Total pembelian setelah diskon : %.f\n", setelahDiskon)
+
+		fmt.Println("\n=========================================================================================")
 	}
-
-	// --- Perhitungan Diskon ---
-	if totalPembelian >= 100000 && totalPembelian < 200000 {
-		diskon = diskon5
-	} else if totalPembelian >= 200000 && totalPembelian < 300000 {
-		diskon = diskon10
-	} else if totalPembelian >= 300000 && totalPembelian < 500000 {
-		diskon = diskon20
-	} else if totalPembelian >= 500000 {
-		diskon = diskon30
-	}else if totalPembelian == 0 || totalPembelian < 100000 {
-		diskon = 0
-	}
-
-	setelahDiskon = float32(totalPembelian) - (float32(totalPembelian) * diskon)
-	setelahDiskon = float32(int(setelahDiskon + 0.5)) // pembulatan ke atas
-
-	// --- Output ---
-	fmt.Println("\n=========================================================================================")
-	// --- Kondisi Pembatalan Pembelian ---
-	if kosong == true {
-		fmt.Println("\n>> ANDA TIDAK MEMBELI BARANG APAPUN ")
-	} else if inputHarga == 0 {
-		fmt.Println("\n>> PEMBELIAN ANDA DIBATALKAN")
-		harga = nil
-		totalPembelian = 0
-		diskon = 0
-		setelahDiskon = 0
-	}
-
-	fmt.Println("\nUrutan Harga :", harga)
-	fmt.Printf("Total Pembelian Anda : %d\n", totalPembelian)
-	fmt.Println("Diskon yang anda peroleh :", diskon*100, "%")
-	fmt.Printf("Total pembelian setelah diskon : %.f\n", setelahDiskon)
-
-	fmt.Println("\n=========================================================================================")
-}
 }
